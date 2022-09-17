6422d46c509eec64a3d0452ad979e3511761b6a9
arnavsurve <arnavsurve@gmail.com>
2022-09-17 15:58:11 -0400

    react and node template with api call

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
 app.get("/api", (req, res) => {
     res.json({message: "hello from server!"});
 });
